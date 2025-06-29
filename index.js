595f02cf0053562b16c1aecf04601cb548974c63
shayan <shayanmuhammad5001@gmail.com>
2025-06-30 01:33:03 +0500

    Comments removed

--- index.js
+++ index.js
@@ -19,39 +19,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// // Get all faculty
-// app.get("/api/faculty", async (req, res, next) => {
-//     try {
-//         const faculty = await sql`SELECT * FROM faculty`;
-//         res.status(200).json(faculty);
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
 app.listen(4500, console.log(`Server is listening on http://localhost:4500`));
-
-/*
-import express  from "express";
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import indexRouter from './routes/index.js'
-    
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const PORT = process.env.PORT || 4500
-
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .use(express.json())
-    .get("/", (req, res)=> res.sendFile(path.join(__dirname, 'index.html')))
-    .use('/api', indexRouter)
-    .listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
-
-
-
-
-
-
-
-*/
